98ebdedabc17da2597dd815bdbf1c4a8c25ca2e4
WaqarKashif00 <waqarkashif00@gmail.com>
2026-03-10 19:38:10 +0500

    chore: only hide firebase API key as environment variable

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,11 @@ export const environment = {
     production: true,
     firebase: {
         apiKey: process.env['NG_APP_FIREBASE_API_KEY'],
-        authDomain: process.env['NG_APP_FIREBASE_AUTH_DOMAIN'],
-        projectId: process.env['NG_APP_FIREBASE_PROJECT_ID'],
-        storageBucket: process.env['NG_APP_FIREBASE_STORAGE_BUCKET'],
-        messagingSenderId: process.env['NG_APP_FIREBASE_MESSAGING_SENDER_ID'],
-        appId: process.env['NG_APP_FIREBASE_APP_ID'],
-        measurementId: process.env['NG_APP_FIREBASE_MEASUREMENT_ID']
+        authDomain: "syntaengineering-54220.firebaseapp.com",
+        projectId: "syntaengineering-54220",
+        storageBucket: "syntaengineering-54220.firebasestorage.app",
+        messagingSenderId: "916719541867",
+        appId: "1:916719541867:web:ec8e0ec00d46f1747c5b2e",
+        measurementId: "G-HBYYF7QR5K"
     }
 };
